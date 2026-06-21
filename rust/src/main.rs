773c700ae94a62e576463f048efdb60e1b8efd37
Muntasir-Contractor <muntasir.contractor06@gmail.com>
2026-06-21 12:09:24 -0400

    Trying out asynchrnous rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use bollard::Docker;
+use std::process::Command;
+use futures_util::future::TryFutureExt;
+
+#[tokio::main]
+async fn main() {
+    let conn = Docker::connect_with_socket_defaults().unwrap();
+    match conn.ping().await{
+        Ok(_) => println!("Connected"),
+        Err(e) => println!("{}",e)
+    };
 }
